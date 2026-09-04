16cae5fe8b83304c55556fd32337d3ea13b15ac0
AbrahamR8A <a20206364@pucp.edu.pe>
2026-09-03 21:11:30 -0500

    commit para guardar

---
+++ src/main/java/com/pucp/lab2/controller/EmployeeController.java
@@ -0,0 +1,20 @@
+package com.pucp.lab2.controller;
+
+import com.pucp.lab2.entity.Employee;
+import com.pucp.lab2.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+}
